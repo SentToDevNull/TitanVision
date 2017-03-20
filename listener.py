b56eb3c1e79d0f4803c396f57720a015c7e2f985
Lukas Yoder <yoderlukas@gmail.com>
2017-03-19 22:04:53 -0400

    fixed minor issue

--- listener.py
+++ listener.py
@@ -2,7 +2,7 @@
 
 ##########################################################################
 #                                                                        #
-#  Copyright (C) 2017  Lukas Yoder and Praneeth Kolicahala               #
+#  Copyright (C) 2017  Lukas Yoder                                       #
 #                                                                        #
 #  This program is free software: you can redistribute it and/or modify  #
 #  it under the terms of the GNU General Public License as published by  #
